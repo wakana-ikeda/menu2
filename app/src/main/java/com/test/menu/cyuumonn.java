b1c9fb549f57935565acb4814c1ccecfb01a4c99
ctc2022 <wkn.aaa.ikeda@gmail.com>
2022-05-17 16:18:03 +0900

    池田コミットします

---
+++ app/src/main/java/com/test/menu/cyuumonn.java
@@ -0,0 +1,14 @@
+package com.test.menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cyuumonn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cyuumonn);
+    }
+}
